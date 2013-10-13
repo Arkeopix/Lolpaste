c81a981a8266c7171e30b94080d1516e88022486
Arkeopix <mendiej750@gmail.com>
2013-10-13 13:45:52 +0200

    stashed values to communicate with template

--- paste.pl
+++ paste.pl
@@ -2,43 +2,42 @@
 
 use Modern::Perl;
 use Mojolicious::Lite;
-use URI::file;
-
 
 get '/' => sub {shift->render('form');};
 
 get '/#foo/#bar' => sub {
     my $self = shift;
     my $path = "./" . $self->param('foo') ."/". $self->param('bar');
+    my $FILE;
 
-    unless (open FILE, '<', $path) {
+    unless (open $FILE, '<', $path) {
 	$self->render(text=>"Could not open file");
     }
-    my $text = <FILE>;
-    close FILE;
-    $self->render(text=>$text);
+    my $text = <$FILE>;
+    close $FILE;
+    $self->render('pasted', poil => $text);
 };
 
 post '/process' => sub {
     my $self = shift;
     my $text = $self->param('Text');
     my $path = "./files/" . $self->param('Title');
-    
-    unless (open FILE, '>', $path) {
+    my $FILE;
+
+    unless (open $FILE, '>', $path) {
 	$self->render(text=>"Could not create/open file");
     }
-    print FILE $text;
-    close FILE;
+    print $FILE $text;
+    close $FILE;
 
-    my $url = URI->new($path);
-    $self->redirect_to($url);
+    $self->redirect_to($path);
 };
 
 app->start;
 
 __DATA__
 
-@@ layouts/default.html.ep
+@@layouts/default.html.ep
 <!doctype html><html>
     <head>
 	<title>LolPaste</title>
@@ -48,9 +47,9 @@ __DATA__
     <body><%= content %></body>
 </html>
 
-@@ form.html.ep
+@@form.html.ep
 
-% layout 'default';
+%layout 'default';
 %=t h1 => 'LolPaste'
     <div id="form" />
     %=t h1 => 'Let the magic happen'
@@ -64,3 +63,13 @@ __DATA__
     %= submit_button 'click', id => 'button'
     %= end
    </div>
+
+@@pasted.html.ep
+
+%layout 'default';
+%=t h1 => 'LolPaste'
+  <p>Here is your paste !</p>
+  <div id="pasted">
+  %= $poil
+  <br/>
+  </div>
