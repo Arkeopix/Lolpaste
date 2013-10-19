cc586478254f2ff25e15838a6da5c58301cf8ee4
Arkeopix <mendiej750@gmail.com>
2013-10-19 17:03:32 +0200

    A little organisation + css works git add paste.pl public/ templates/

--- paste.pl
+++ paste.pl
@@ -13,7 +13,7 @@ get '/#foo/#bar' => sub {
     unless (open $FILE, '<', $path) {
 	$self->render(text=>"Could not open file");
     }
-    my $text = <$FILE>;
+    my $text = join "", <$FILE>;
     close $FILE;
     $self->render('pasted', poil => $text);
 };
@@ -23,7 +23,7 @@ post '/process' => sub {
     my $text = $self->param('Text');
     my $path = "./files/" . $self->param('Title');
     my $FILE;
-
+    
     unless (open $FILE, '>', $path) {
 	$self->render(text=>"Could not create/open file");
     }
@@ -35,41 +35,3 @@ post '/process' => sub {
 
 app->start;
 
-__DATA__
-
-@@layouts/default.html.ep
-<!doctype html><html>
-    <head>
-	<title>LolPaste</title>
-	<link type="text/css" rel="stylesheet" href="style.css" />
-	<link href='http://fonts.googleapis.com/css?family=Autour+One' rel='stylesheet' type='text/css'>
-    </head>
-    <body><%= content %></body>
-</html>
-
-@@form.html.ep
-
-%layout 'default';
-%=t h1 => 'LolPaste'
-    <div id="form" />
-    %=t h1 => 'Let the magic happen'
-    %= form_for '/process' => (method => 'post') => begin
-    %= label_for Title => 'Title:' 
-    %= text_field 'Title'
-    <br/>
-    %= label_for Text => 'Text:'
-    %= text_area 'Text', rows => 10, id =>'flex'
-    <br/>
-    %= submit_button 'click', id => 'button'
-    %= end
-   </div>
-
-@@pasted.html.ep
-
-%layout 'default';
-%=t h1 => 'LolPaste'
-  <p>Here is your paste !</p>
-  <div id="pasted">
-  %= $poil
-  <br/>
-  </div>
