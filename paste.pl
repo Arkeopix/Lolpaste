6b3496c8a16a4d2a1607e1f44e54ef55c6e37024
Arkeopix <mendiej750@gmail.com>
2016-02-07 11:52:17 +0100

    refactoring some code to be more secure, plain file to db, kicked out css, added type select

--- paste.pl
+++ paste.pl
@@ -1,37 +1,54 @@
 #! /usr/bin/perl
-
-use Modern::Perl;
+use strict;
+use warnings;
 use Mojolicious::Lite;
+use Digest::MD5 qw( md5_hex );
+use Redis;
 
-get '/' => sub {shift->render('form');};
+#####################################################################
+# App setup                                                         #
+#####################################################################
+# this is for signed cookies, but there is none so this is just
+# to prevent stupid debug messages
+app->secrets( [ 'super secret sauce', 'sauce super secret' ] );
 
-get '/#foo/#bar' => sub {
+# Automatically name log files development and set log lvl (debug | info)
+app->mode('development');
+
+# a nicer access to redis than just a global variable
+helper redis =>
+  sub { state $redis = Redis->new( server => '127.0.0.1:6379' ); };
+
+#####################################################################
+# App routes                                                        #
+#####################################################################
+get '/' => sub { shift->render('form'); };
+get '/#digest_id' => \&display_paste;
+post '/process'   => \&process_post;
+
+#####################################################################
+# Controllers                                                       #
+#####################################################################
+
+sub display_paste {
     my $self = shift;
-    my $path = "./" . $self->param('foo') ."/". $self->param('bar');
-    my $FILE;
 
-    unless (open $FILE, '<', $path) {
-	$self->render(text=>"Could not open file");
-    }
-    my $text = join "", <$FILE>;
-    close $FILE;
-    $self->render('pasted', poil => $text);
-};
+    my $file = $self->redis->hgetall( $self->param('digest_id') );
+    $self->render( 'pasted', file_info => $file );
+}
 
-post '/process' => sub {
-    my $self = shift;
-    my $text = $self->param('Text');
-    my $path = "./files/" . $self->param('Title');
-    my $FILE;
-    
-    unless (open $FILE, '>', $path) {
-	$self->render(text=>"Could not create/open file");
-    }
-    print $FILE $text;
-    close $FILE;
+sub process_post {
+    my $self           = shift;
+    my $title          = $self->param('Title');
+    my $type           = $self->param('Type');
+    my $text           = $self->param('Text');
+    my $digest_post_id = md5_hex( $title . $type . $text . time );
 
-    $self->redirect_to($path);
-};
+    $self->redis->hset( $digest_post_id, 'title', $title );
+    $self->redis->hset( $digest_post_id, 'type',  $type );
+    $self->redis->hset( $digest_post_id, 'text',  $text );
+    return $self->redirect_to("/$digest_post_id");
+}
 
 app->start;
 
